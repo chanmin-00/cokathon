eef2b6062af6bb718a91e5045d32d603e0ce0174
jeongchanmin <jjw421412@naver.com>
2025-07-23 14:57:34 +0900

    스프링 초기 설정

---
+++ src/main/java/com/example/cokathon/CokathonApplication.java
@@ -0,0 +1,13 @@
+package com.example.cokathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CokathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CokathonApplication.class, args);
+	}
+
+}
